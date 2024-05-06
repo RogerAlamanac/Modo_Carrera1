d0550c6bc9390cc65c8ea54a4a5e700cc205c4d4
RogerAlamanac <roger.alamanac@enti.cat>
2024-05-06 14:14:46 +0200

    Avenços

--- Modo_Carrera/Modo_Carrera/Team.cpp
+++ Modo_Carrera/Modo_Carrera/Team.cpp
@@ -1,1 +1,9 @@
 #include "Team.h"
+#include <iostream>
+
+const int NUM_PLAYERS_INITIAL = 20;
+void Team::InicializeTeam() {
+	for (int i = 0; i < NUM_PLAYERS_INITIAL; ++i) {
+		std::cin >> player.name;
+	}
+}
--- Modo_Carrera/Modo_Carrera/Team.h
+++ Modo_Carrera/Modo_Carrera/Team.h
@@ -1,5 +1,22 @@
 #pragma once
-struct Team {
 
+enum class Positions{
+	STRIKER,
+	WINGER,
+	MIDFIELDER,
+	FULLBACK,
+	CENTERBACK,
+	GOALKEEPER
+};
+struct Team {
+	int numPlayers;
+	Player player;
+	int numTitles;
+	void InicializeTeam();
 };
 
+struct Player {
+	char name[20];
+	Positions position;
+	int number;
+};
