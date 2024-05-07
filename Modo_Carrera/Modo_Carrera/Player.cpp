4aa3d84ac595d13b0ea44904949b73e0eb23d2d5
RogerAlamanac <roger.alamanac@enti.cat>
2024-05-07 12:03:21 +0200

    Okey

--- Modo_Carrera/Modo_Carrera/Player.cpp
+++ Modo_Carrera/Modo_Carrera/Player.cpp
@@ -1,3 +1,8 @@
 #include "Player.h"
 
-
+Player::Player() {
+	name = rand() % ('Z' - 'A' + 1) + 'A';
+	age = rand() % (43 - 16 + 1) + 16;
+	OVR = rand() % (80 - 60 + 1) + 60;
+	number = rand() % (30 - 2 + 1) + 1;
+}
--- Modo_Carrera/Modo_Carrera/Team.cpp
+++ Modo_Carrera/Modo_Carrera/Team.cpp
@@ -6,15 +6,7 @@ const int NUM_PLAYERS_INITIAL = 20;
 void Team::InicializeTeam() {
 	Player player[NUM_PLAYERS_INITIAL];
 	for (int i = 0; i < NUM_PLAYERS_INITIAL; ++i) {
-		player[i].name = rand()%('Z'-'A'+1)+'A';
-		std::cout << "Name: " << player[i].name << std::endl;
-		player[i].age = rand() % (43 - 16 + 1) + 16; 
-		std::cout << "Age: " << player[i].age << std::endl;
-		player[i].OVR = rand() % (80 - 60 + 1) + 60;
-		std::cout << "OVR: " << player[i].OVR << std::endl;
-		player[i].number = rand() % (30 - 2 + 1) + 1;
-		std::cout << "Number: " << player[i].number << std::endl;
-		std::cout << "-------------------" << std::endl;
+		
 	}
 }
 
